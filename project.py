cb14952748a3a5f38d3e81eeaae4a1386ed95477
Creaghixc <isaiahcreagh@yahoo.com>
2023-08-02 08:44:18 -0500

    added csv and started project

---
+++ project.py
@@ -0,0 +1,10 @@
+print("*******************Main Menu*****************")
+print("1. Read CSV file of grades")
+print("2. Generate student report file")
+print("3. Generate student report charts")
+print("4. Generate class report file")
+print("5. Generate class report charts")
+print("6. Quit")
+print("************************************************")
+
+option = input("")
